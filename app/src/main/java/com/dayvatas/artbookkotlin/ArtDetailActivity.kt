1f02c1c655bd8e317286608ec9676356d5c9bd79
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-04-30 23:18:16 +0300

    Gallery permissions added

--- app/src/main/java/com/dayvatas/artbookkotlin/ArtDetailActivity.kt
+++ app/src/main/java/com/dayvatas/artbookkotlin/ArtDetailActivity.kt
@@ -1,24 +1,55 @@
 package com.dayvatas.artbookkotlin
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import com.dayvatas.artbookkotlin.databinding.ActivityArtDetailBinding
+import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 
 class ArtDetailActivity : AppCompatActivity() {
     private lateinit var binding : ActivityArtDetailBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityArtDetailBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        registerLauncher()
     }
 
-    fun selectImage(){
+    fun selectImage(view : View){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            //rationale
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
+                  //request permission
+
+                })
+            }
+
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //intent
+        }
+
 
     }
 
-    fun saveClicked(){
+    fun saveClicked(view : View){
 
+    }
+
+    private fun registerLauncher(){
+//        activityResultLauncher = registerForActivityResult(startActivityForResult())
     }
 }
