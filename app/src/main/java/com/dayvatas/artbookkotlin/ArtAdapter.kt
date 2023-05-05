8327327ae2b8ca5c0491378c46a78004ea2b1159
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-05 22:54:19 +0300

    Recycler Adapter

---
+++ app/src/main/java/com/dayvatas/artbookkotlin/ArtAdapter.kt
@@ -0,0 +1,24 @@
+package com.dayvatas.artbookkotlin
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dayvatas.artbookkotlin.databinding.RecyclerRowBinding
+
+class ArtAdapter() : RecyclerView.Adapter<ArtAdapter.ArtHolder>() {
+    class ArtHolder(val binding : RecyclerRowBinding): RecyclerView.ViewHolder(binding.root){
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ArtHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
