28a882738695a4e2d9a9ac4c1d37dc098aed0364
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-07 22:17:52 +0300

    Recycler Adapter

--- app/src/main/java/com/dayvatas/artbookkotlin/ArtAdapter.kt
+++ app/src/main/java/com/dayvatas/artbookkotlin/ArtAdapter.kt
@@ -4,7 +4,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dayvatas.artbookkotlin.databinding.RecyclerRowBinding
 
-class ArtAdapter() : RecyclerView.Adapter<ArtAdapter.ArtHolder>() {
+class ArtAdapter(val artList : ArrayList<Art>) : RecyclerView.Adapter<ArtAdapter.ArtHolder>() {
     class ArtHolder(val binding : RecyclerRowBinding): RecyclerView.ViewHolder(binding.root){
 
 
