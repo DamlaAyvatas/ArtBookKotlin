59b6e82b0c4fc774f352407864eb4786b4af8872
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-09 18:09:14 +0300

    Recycler Adapter

--- app/src/main/java/com/dayvatas/artbookkotlin/ArtAdapter.kt
+++ app/src/main/java/com/dayvatas/artbookkotlin/ArtAdapter.kt
@@ -1,8 +1,11 @@
 package com.dayvatas.artbookkotlin
 
+import android.content.Intent
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dayvatas.artbookkotlin.databinding.RecyclerRowBinding
+
 
 class ArtAdapter(val artList : ArrayList<Art>) : RecyclerView.Adapter<ArtAdapter.ArtHolder>() {
     class ArtHolder(val binding : RecyclerRowBinding): RecyclerView.ViewHolder(binding.root){
@@ -11,7 +14,8 @@ class ArtAdapter(val artList : ArrayList<Art>) : RecyclerView.Adapter<ArtAdapter
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtHolder {
-        TODO("Not yet implemented")
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ArtHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -19,6 +23,10 @@ class ArtAdapter(val artList : ArrayList<Art>) : RecyclerView.Adapter<ArtAdapter
     }
 
     override fun onBindViewHolder(holder: ArtHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.recyclerRowText.text = artList.get(position).name
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView.context, ArtDetailActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 }
